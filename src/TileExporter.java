8d413b8b0b3267f6088668c88d8f1dae4251d2a1
agune001 <adi.vtca@gmail.com>
2018-04-15 17:13:50 -0700

    get extent works

--- src/TileExporter.java
+++ src/TileExporter.java
@@ -7,125 +7,52 @@ import java.io.*;
 import java.util.*;
 import org.apache.commons.lang3.ArrayUtils;
 
-public class DBCreator {
+public class TileExporter {
 
     static final String DB_URL = "jdbc:postgresql://localhost:5432/";
     static final String USER = "postgres";
     static final String PASS = "pass";
 
     public static void main(String []args) throws IOException {
-        if(args.length < 2){
+        /*if(args.length < 2){
             System.out.println("Error: please call in the format");
             System.out.println("java -jar /citygml-tiling-app.jar \"path/to/gml\" \"path/to/createdb.bat\"");
             return;
-        }
-
-
+        }*/
+        Connection conn = null;
+        Statement stmt = null;
+        getExtents(stmt,conn, "nyc");
 
     }
 
-    private static String[] getExtents(Statement stmt, Connection conn, String dbname, String batpath, String[] errors) {
+    private static String getExtents(Statement stmt, Connection conn, String dbname) {
         String DBNAME = dbname;
-
+        String extents = "";
         try {
             //Class.forName("org.postgresql.Driver");
 
             conn = DriverManager.getConnection(DB_URL + DBNAME, USER, PASS);
             stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("Select ST_Extent(ST_Transform(envelope, 4326)) from citydb.cityobject where envelope is not null");
-            boolean done = false;
-            String postgis = "";
+            ResultSet rs = stmt.executeQuery("Select ST_Extent(ST_Transform(envelope, 4326)) e from citydb.cityobject where envelope is not null");
+
+
             while(rs.next())
             {
-                postgis = rs.getString("e");
-                System.out.println("Checking postgis version " + postgis);
+                extents = rs.getString("e");
+                System.out.println("Extents are " + extents);
             }
             stmt.close();
             conn.close();
-            System.out.println("Initializing database...");
-            Thread.sleep(3000);
-
-            while(!done){
-                if(!StringUtils.isNullOrEmpty(postgis)){
-                    done = true;
-                    applyCityDBSchema(batpath, dbname);
-                }
-            }
-
-
 
 
         } catch (SQLException ex) {
             System.err.println("SQLException: " + ex.getMessage());
             System.err.println("SQLState: " + ex.getSQLState());
             System.err.println("VendorError: " + ex.getErrorCode());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
-        return checkDB(stmt, conn, dbname, errors);
+        return extents;
     }
 
-    private static void applyCityDBSchema(String batpath, String dbname)
-    {
-        System.out.println("Attepmpting to apply CityDB schema to " + dbname );
-        if(batpath.substring(batpath.length() - 1).equals("\"")){
-            batpath = batpath.substring(0, batpath.length() - 1);
-        }
-        try {
-            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "START", "CREATE_DB.bat");
-            File dir = new File(batpath);
-            pb.directory(dir);
-            Process p = null;
-            p = pb.start();
-            Thread.sleep(3000);
-        } catch (IOException e) {
-            System.out.println("Could not create CityDB");
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Applying CityDB schema...");
 
-    }
-    private static void updateBatFile(String path, String update) throws IOException
-    {
-        //regex: CITYDB=(.*)$
-        File originalFile = new File(path + "\\CREATE_DB.bat");
-        BufferedReader br = new BufferedReader(new FileReader(originalFile));
-
-        // Construct the new file that will later be renamed to the original
-        // filename.
-        File tempFile = new File(path + "\\CREATE_DB-TEMP.bat");
-        PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
-
-        String line = null;
-        // Read from the original file and write to the new
-        // unless content matches data to be removed.
-        while ((line = br.readLine()) != null) {
-
-            if (line.contains("set CITYDB")) {
-
-                String strCurrentDB = line.substring(line.lastIndexOf("="), line.length());
-                line = line.substring(0,line.lastIndexOf("=")) + "="+ update;
-                System.out.println("Current line: " + strCurrentDB + "; Replaced with: " + update);
-            }
-
-            pw.println(line);
-            pw.flush();
-        }
-        pw.close();
-        br.close();
-
-        // Delete the original file
-        if (!originalFile.delete()) {
-            System.out.println("Could not delete file");
-            return;
-        }
-
-        // Rename the new file to the filename the original file had.
-        if (!tempFile.renameTo(originalFile))
-            System.out.println("Could not rename file");
-
-    }
 
 }
